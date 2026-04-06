99217ffcd72f5593b8932d902e55e0669e50c400
Maxessien <essienmax484@gmail.com>
2026-04-06 16:21:42 +0100

    Initialised tauri project

---
+++ src-tauri/gen/android/app/src/main/java/com/max_essien/mx_send_tauri/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.max_essien.mx_send_tauri
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
